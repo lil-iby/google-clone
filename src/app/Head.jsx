16ee54499db58078e404c940ece32058a42793ac
guemkan <dietrichguemkan38@gmail.com>
2024-12-22 19:33:24 +0000

    install next js and first commit

---
+++ src/app/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+      <title>Google Clone Next js 15</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport'/>
+      <meta name='description' content='Google clone create by NextJs 15'/>
+      <link rel='icon' href='/favicon.ico'/>
+    </>
+  )
+}
