a253bf166661f9eb7fc567496e9031e4e50c7532
guemkan <dietrichguemkan38@gmail.com>
2024-12-23 04:50:02 +0000

    add home header

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
-
+import HomeHeader from "@/components/HomeHeader"
 export default function Home() {
   return (
-    <div className="text-red-600 text-3xl">Home</div>
+    <> 
+      <HomeHeader />
+    </>
   )
 }
